1e3b8c20986d284bb489b093daca36c68aac4d0a
rafaceschin <rafaceschin@gmail.com>
2017-01-11 10:11:52 -0500

    Working network, saving parameters work, and basic metrics plotting

---
+++ plot_multi_cost.py
@@ -0,0 +1,67 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import json
+
+#iWantHue selection of 20 distinct colors
+
+colors = ["#bd8a79", "#7a3fcf", "#73d350",
+          "#cf52c3", "#ccd049", "#522a7e",
+          "#67d7a6", "#d84c3c", "#7778d6",
+          "#c88938", "#556891", "#c8d099",
+          "#51284a", "#5d7c37", "#c54a7c",
+          "#52897d", "#7c3729", "#88c3d9",
+          "#3b3c2d", "#cd9fca"]
+
+def load_csv(csv_file):
+    data = []
+    with open(csv_file, 'rb') as f:
+        reader = csv.reader(f, delimiter=',')
+        headers = reader.next()
+        for row in reader:
+            data.append(row)
+    return np.asarray(data)
+
+def stack_data(csv_list, idx):
+    data = []
+    for i in xrange(len(csv_list)):
+        data.append(csv_list[i][:,idx])
+    return data
+
+def plot_metric(data,labels,
+                title="Mean Cost",
+                sfile='/home/rafa/Desktop/plot',
+                ylab='Mean Epoch Cost (log scale)'):
+    fig1=plt.figure(figsize=(10,6))
+    fig1.set_facecolor([1,1,1])
+    ax=fig1.add_subplot(111)
+    for i in xrange(len(data)):
+        x=np.arange(0, data[i].shape[0])
+        ax.plot(x, data[i][:], lw=3, color=colors[i], alpha=0.8, label=labels[i])
+    ax.set_xlim(0, 200)
+    #ax.set_ylim(axis[2],axis[3])
+    ax.set_yscale('log')
+    ax.set_title(title, fontsize=14, fontweight='bold')
+    ax.set_xlabel('Epoch')
+    ax.set_ylabel(ylab)
+    ax.legend()
+    fig1.savefig(sfile, dpi=300, facecolor=fig1.get_facecolor(), edgecolor='w',
+                orientation='landscape', bbox_inches=None, pad_inches=0.1)
+
+
+if __name__ == '__main__':
+    parent_dir = '/home/rafa/Dropbox/ws_Python/NN/CRBnet/'
+    save_file = parent_dir+'cost.png'
+
+    r11 = load_csv(parent_dir+'Run_11/accuracy_1.csv')
+    r12 = load_csv(parent_dir+'Run_12/accuracy.csv')
+    r13 = load_csv(parent_dir+'Run_13/accuracy.csv')
+    r14 = load_csv(parent_dir+'Run_14/accuracy.csv')
+    r15 = load_csv(parent_dir+'Run_15/accuracy.csv')
+
+    csvs = [r11, r12, r13, r14, r15]
+    labels = ['1 vox/1 deg', '5 vox/1 deg', '5 vox/5 deg', '10 vox/10 deg', '5-10 vox/ 5-10 deg']
+    data = stack_data(csvs, 4)
+    plot_metric(data,labels, title="Mean Epoch Cost", sfile=save_file)
+
--- run_network.py
+++ run_network.py
@@ -1,44 +1,148 @@
+"""
+Reads network parameters from json file.
+
+
+## Layer syntax from code:
+## image_shape = (mini batch size,
+##                number of input dimensions/channels/feature maps from previous layer (or 1 for initial input),
+##               Image X,Y,Z)
+
+## Filter_shape= (number of filters i.e. how many new feature maps to create in this layer,
+##                input feature maps: number of input dimensions/feature maps from previous layer (or initial input),
+##                filter X,Y,Z: x,y,z of local receptive field)
+
+## After a conv layer:
+## Next layer input size will be:
+## X - x_filter + 1, Y - y_filter + 1, Z - z_filter + 1
+## Then divide by pool size
+## For a fully connected layer, multiply these together with the number of feature maps in the previous layer
+## (if following a ConvPool layer)
+"""
+import json
+import network as nn
+import time
+import numpy as np
+import sys,os
+
+class build_network(object):
+    def __init__(self, network_file):
+
+        with open(network_file) as net_data:
+            self.network = json.load(net_data)
+
+        self.layers = [self.build_init(self.network['input_dims'])]
+        self.layers_construct = []
+        self.logfile =  os.path.dirname(network_file)+'/accuracy.csv'
+        self.params_file = os.path.dirname(network_file)+'/params.save'
+        self.restart = self.network['restart']
+
+        for n_layer in xrange(len(self.network['Structure'])):
+            lid = "layer_{0}".format(n_layer)
+            layer =  self.network['Structure'][lid]
+
+            if  layer['type'] == 'Conv':
+                lout = self.build_conv(layer)
+
+            elif layer['type'] == 'Full':
+                lout = self.build_full(layer)
+
+            elif layer['type'] == 'Softmax':
+                lout = self.build_soft(layer)
+
+            self.layers.append(lout)
+            self.layers_construct.append(lout['construct'])
+
+
+        self.training_data, self.validation_data, self.test_data = nn.load_data_shared(
+            self.network['Data']['TRAIN_STACK'], self.network['Data']['TRAIN_LABELS'],
+            self.network['Data']['VALID_STACK'], self.network['Data']['VALID_LABELS'],
+            self.network['Data']['TEST_STACK'], self.network['Data']['TEST_LABELS'])
+
+
+
+    def build_init(self, layer):
+        out_shape = np.asarray(self.network['input_dims'])
+        return {'out_dims' : out_shape}
+
+
+
+    def build_conv(self, layer):
+
+        in_shape = self.layers[-1]['out_dims']
+        feature_maps = np.asarray(layer['feature_maps'])
+        image_shape=[[self.network['mini_batch_size']], in_shape.tolist()]
+        filter_shape=([feature_maps.tolist()], [in_shape[0]], layer['lrf'])
+
+        construct = nn.ConvPoolLayer(
+            image_shape=tuple([param for sublist in image_shape for param in sublist]),
+            filter_shape=tuple([param for sublist in filter_shape for param in sublist]),
+            poolsize=tuple(layer['max_pool']),
+            activation_fn=layer['activation_function'])
+
+        out_shape = ((in_shape[1:] - np.asarray(layer['lrf'])) + 1) / np.asarray(layer['max_pool'])
+
+        out_dims = np.append(feature_maps, out_shape)
+
+        layer = {'construct':construct,
+                     'out_dims' : out_dims,}
+        return layer
+
+
+    def build_full(self, layer):
+
+        in_shape = np.product(self.layers[-1]['out_dims'])
+        out_shape = np.asarray(layer['n_out'])
+
+        construct = nn.FullyConnectedLayer(
+            n_in= in_shape,
+            n_out = layer['n_out'],
+            activation_fn = layer['activation_function'],
+            p_dropout = layer['p_dropout']
+        )
+
+        layer = {'construct' : construct,
+                 'out_dims' : out_shape,}
+        return layer
+
+    def build_soft(self, layer):
+
+        in_shape = np.product(self.layers[-1]['out_dims'])
+        out_shape = np.asarray(layer['n_out'])
+
+        construct = nn.SoftmaxLayer(
+            n_in= in_shape,
+            n_out = layer['n_out'],
+            p_dropout = layer['p_dropout']
+        )
+
+        layer = {'construct' : construct,
+                 'out_dims' : out_shape,}
+        return layer
+
+    def run(self):
+#        print(self.layers_construct)
+
+        net = nn.Network(self.layers_construct,
+                         self.network['mini_batch_size'],
+                         params_file = self.params_file,
+                         logfile=self.logfile,
+                         restart=self.restart)
+        net.SGD(self.training_data, self.network['epochs'],
+                self.network['mini_batch_size'], self.network['eta'],
+                self.validation_data, self.test_data, self.network['lmbda'])
+
+
+
 if __name__ == '__main__':
 
-    import network as nn
-    import theano.tensor as T
-    import time
-
- #   def ReLU(z): return T.maximum(0.0, z)
-
-    mini_bach_size = 10
+    network_file = os.path.abspath(sys.argv[1])
 
     print "started at: " + time.asctime()
     t1_asc = time.asctime()
     t1 = time.time()
 
-## Layer syntax:
-## image_shape = (mini batch size,
-##                number of input dimensions/feature maps,
-##               Image height, Image width)
-
-## Filter_shape= (number of filters i.e. how many new feature maps,
-##                input feature maps: number of input dimensions/feature maps,
-##                filter H,W: h,w of local receptive field)
-
-    training_data, validation_data, test_data = nn.load_data_shared()
-    net = nn.Network([
-                nn.ConvPoolLayer(image_shape=(mini_bach_size, 1, 28, 28),
-                                 filter_shape=(20, 1, 5, 5),
-                                 poolsize=(2,2),
-                                 activation_fn="ReLU"),
-                nn.ConvPoolLayer(image_shape=(mini_bach_size, 20, 12, 12),
-                                 filter_shape=(40, 20, 5, 5),
-                                 poolsize=(2,2),
-                                 activation_fn="ReLU"),
-                nn.FullyConnectedLayer(n_in=40*4*4, n_out=1000,
-                                       activation_fn="ReLU", p_dropout=0.5),
-                nn.FullyConnectedLayer(n_in=1000, n_out=1000,
-                                       activation_fn="ReLU", p_dropout=0.5),
-                nn.SoftmaxLayer(n_in=1000, n_out=10,p_dropout=0.5)], mini_bach_size)
-    net.SGD(training_data, 40, mini_bach_size, 0.03,
-            validation_data, test_data)
-
+    net = build_network(network_file)
+    net.run()
 
     print "started at: " + t1_asc
     print "finished at: " + time.asctime()
